231eb757d0eabc4503ed0d2f80693f375424bef4
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-02 20:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const makeChange = (c) => {
   // your name here
+	let q = c%25>0?Math.floor(c/25):0;
+	c = c%25;
+	let d = c%10>0?Math.floor(c/10):0;
+	c = c%10;
+	let n = c%5>0?Math.floor(c/5):0;
+	c = c%5;
+	let p = Math.floor(c/1);
+	return {q,d,n,p};
 };
 
 // Do not the change the code below
